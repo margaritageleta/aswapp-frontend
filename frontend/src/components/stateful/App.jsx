8dd92cd79dd330555d0d98aec17b5d2f0ad1a3d5
Rita <rita.geleta@jediupc.com>
2020-12-14 15:01:33 +0100

    Add app screens

--- frontend/src/components/stateful/App.jsx
+++ frontend/src/components/stateful/App.jsx
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
 // import { BrowserRouter as Router } from 'react-router-dom';
-// the Provider provides the store
-import store from '../../redux/store';
-import { Provider } from 'react-redux';
 import Items from './Items';
 import Item from './Item';
 import Comments from './Comments';
 import User from './User';
 import Nav from '../stateless/Nav';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import TypoGraphy from '@material-ui/core/Typography';
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-  
-  render() {
-    return (
-      <Provider store={ store }>
-        <AppBar color="primary" position="static" style={{ margin: 0 }}>
-          <Toolbar>
-            <TypoGraphy variant="title" color="inherit">
-              HackerNews
-            </TypoGraphy>
-          </Toolbar>
-        </AppBar>
-        <div>
-          <header>
-            <Items type={'ALL'} userId={''} voted={false} />
+const News = () => {
+	return (
+		<div>
+        <Nav/>
+				<Items type={'ALL'} userId={''} voted={false} />
+		</div>
+	)
+};
 
-          </header>
-        </div>
-      </Provider>
-    );
-  }
-}
+const Newest = () => {
+	return (
+		<div>
+        <Nav/>
+				<Items type={'URL'} userId={''} voted={false} />
+		</div>
+	)
+};
 
-export default App;
+const Ask = () => {
+	return (
+		<div>
+        <Nav/>
+				<Items type={'ASK'} userId={''} voted={false} />
+		</div>
+	)
+};
+
+export { News, Newest, Ask };
